7dba17320ae1ade15d78d811657d1e6590c3fee3
Tyler Williams <tylerwilliams3000@gmail.com>
2025-10-01 07:10:41 -0500

    Use more efficient test context

--- OddJava/src/test/groovy/com/oddball/challenges/BadDayServiceSpec.groovy
+++ OddJava/src/test/groovy/com/oddball/challenges/BadDayServiceSpec.groovy
@@ -2,19 +2,19 @@ package com.oddball.challenges
 
 import com.oddball.challenges.mood.Mood
 import com.oddball.challenges.stress.Stress
-import com.oddball.challenges.stress.StressRepository
 import com.oddball.challenges.user.User
 import jakarta.persistence.EntityManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 
 import javax.sql.DataSource
 import java.time.LocalDate
 
-@SpringBootTest
+@DataJpaTest
+@Import(BadDayService)
 class BadDayServiceSpec extends Specification {
     @Autowired
     BadDayService badDayService
