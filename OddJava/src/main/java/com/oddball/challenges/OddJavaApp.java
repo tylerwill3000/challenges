a1143c73bb9a04795bb3284d1bbb622580bbf969
Tyler Williams <tylerwilliams3000@gmail.com>
2025-10-17 18:00:23 -0500

    Resolve weak warning

--- OddJava/src/main/java/com/oddball/challenges/OddJavaApp.java
+++ OddJava/src/main/java/com/oddball/challenges/OddJavaApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OddJavaApp {
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(OddJavaApp.class, args);
 	}
 }
